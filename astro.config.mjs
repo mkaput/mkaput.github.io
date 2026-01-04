3d3881a9b067fd390c18000a6bb419977192152c
Marek Kaput <marek.kaput@outlook.com>
2026-01-04 19:48:08 +0100

    Revert astro-seo usage

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://murek.dev",
+  integrations: [tailwind(), sitemap()],
+});
