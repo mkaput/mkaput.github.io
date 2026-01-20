f1652c716bd8c33764b05ae2559a8ee8c8e09c38
Marek Kaput <marek.kaput@outlook.com>
2026-01-20 22:31:48 +0100

    fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,42 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://murek.dev",
   integrations: [sitemap()],
+  experimental: {
+    fonts: [
+      {
+        name: "Gambarino",
+        cssVariable: "--font-gambarino",
+        fallbacks: [
+          "Didot",
+          "Bodoni MT",
+          "Noto Serif Display",
+          "URW Palladio L",
+          "P052",
+          "Sylfaen",
+          "serif",
+        ],
+        provider: fontProviders.fontshare(),
+      },
+      {
+        name: "Switzer",
+        cssVariable: "--font-switzer",
+        fallbacks: [
+          "Inter",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial Nova",
+          "Nimbus Sans",
+          "Arial",
+          "sans-serif",
+        ],
+        provider: fontProviders.fontshare(),
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
